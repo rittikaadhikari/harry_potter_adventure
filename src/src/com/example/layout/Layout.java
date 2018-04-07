d2548905ff0f26221e5c5ff9751c5d4473b7550f
rittikaadhikari <rittika.adhikari@gmail.com>
2018-04-06 21:53:53 -0500

    Started writing layout files

--- src/src/com/example/layout/Layout.java
+++ src/src/com/example/layout/Layout.java
@@ -1,4 +1,37 @@
 package src.com.example.layout;
 
+import src.com.example.things.Monster;
+import src.com.example.things.Player;
+import src.com.example.things.Student;
+
+import java.util.ArrayList;
+
 public class Layout {
+    public String startingRoom;
+    public ArrayList<Room> rooms;
+    public ArrayList<Student> students;
+    public ArrayList<Monster> monsters;
+    public Player player;
+
+    public String getStartingRoom() {
+        return startingRoom;
+    }
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public ArrayList<Monster> getMonsters() {
+        return monsters;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+
 }
--- src/src/com/example/layout/Room.java
+++ src/src/com/example/layout/Room.java
@@ -1,4 +1,28 @@
 package src.com.example.layout;
 
+import src.com.example.things.Thing;
+
+import java.util.ArrayList;
+
 public class Room {
+    public String name;
+    public String description;
+    public ArrayList<Thing> items;
+    public ArrayList<String> directions;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ArrayList<Thing> getItems() {
+        return items;
+    }
+
+    public ArrayList<String> getDirections() {
+        return directions;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
