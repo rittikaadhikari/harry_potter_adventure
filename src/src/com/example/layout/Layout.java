e39294e043fb01a586856482afa0d218685ae60b
Sejal K. Parmar <sejalkp2@illinois.edu>
2018-04-06 23:00:30 -0500

    added comments to layout classes

--- src/src/com/example/layout/Layout.java
+++ src/src/com/example/layout/Layout.java
@@ -3,47 +3,105 @@ package src.com.example.layout;
 import com.google.gson.Gson;
 import src.com.example.Data;
 import src.com.example.things.*;
+
 import java.util.ArrayList;
 
 public class Layout {
+    /**
+     * the room you start in
+     */
     public String startingRoom;
+    /**
+     * all the room objects
+     */
     public ArrayList<Room> rooms;
+    /**
+     * all the student objects
+     */
     public ArrayList<Student> students;
+    /**
+     * all the monster objects
+     */
     public ArrayList<Monster> monsters;
+    /**
+     * all the food objects
+     */
     public ArrayList<Food> food;
+    /**
+     * all the spell objects
+     */
     public ArrayList<Spell> spells;
+    /**
+     * the player object
+     */
     public Player player;
 
+    /**
+     * gets starting room.
+     *
+     * @return starting room as String
+     */
     public String getStartingRoom() {
         return startingRoom;
     }
 
+    /**
+     * gets all the rooms in an arraylist.
+     *
+     * @return rooms
+     */
     public ArrayList<Room> getRooms() {
         return rooms;
     }
 
+    /**
+     * gets all students in an arraylist.
+     *
+     * @return students
+     */
     public ArrayList<Student> getStudents() {
         return students;
     }
 
+    /**
+     * gets all monsters in arraylist.
+     *
+     * @return monsters
+     */
     public ArrayList<Monster> getMonsters() {
         return monsters;
     }
 
+    /**
+     * gets the player object
+     *
+     * @return player
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * gets all food in an arraylist.
+     *
+     * @return food
+     */
     public ArrayList<Food> getFood() {
         return food;
     }
 
+    /**
+     * gets all spells in an arraylist.
+     *
+     * @return spells
+     */
     public ArrayList<Spell> getSpells() {
         return spells;
     }
 
     /**
      * Loads the layout from a given filename.
+     *
      * @param filename given filename
      * @return a Layout object from JSON
      */
--- src/src/com/example/layout/Room.java
+++ src/src/com/example/layout/Room.java
@@ -6,33 +6,63 @@ import src.com.example.things.Thing;
 import java.util.ArrayList;
 
 public class Room {
+    /** name of the room */
     public String name;
+    /** the room's description */
     public String description;
+    /** the items the room contains */
     public ArrayList<Item> items;
+    /** where you can go from this room */
     public ArrayList<String> directions;
+    /** students you can duel, if any */
     public ArrayList<String> studentsInRoom;
+    /** monsters you can duel, if any */
     public ArrayList<String> monstersInRoom;
 
+    /**
+     * gets room description.
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * gets room's items.
+     * @return items
+     */
     public ArrayList<Item> getItems() {
         return items;
     }
 
+    /**
+     * gets where you can go from this room.
+     * @return directions
+     */
     public ArrayList<String> getDirections() {
         return directions;
     }
 
+    /**
+     * gets room name.
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * gets all the students in the room.
+     * @return students
+     */
     public ArrayList<String> getStudentsInRoom() {
         return studentsInRoom;
     }
 
+    /**
+     * gets all the monsters in the room.
+     * @return monsters
+     */
     public ArrayList<String> getMonstersInRoom() {
         return monstersInRoom;
     }
